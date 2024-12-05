d7e7cf89ed466e47985410bb0c04ffa588cf5c0c
Sachin-Bandaranayaka <sachinbandaranayaka505@gmail.com>
2024-12-05 14:37:56 +0530

    fix: downgrade Firebase to v9.23.0 for better Next.js compatibility

--- next.config.js
+++ next.config.js
@@ -15,19 +15,6 @@ const nextConfig = {
       allowedOrigins: ['localhost:3000', 'synapse-labs.onrender.com'],
     },
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        crypto: require.resolve('crypto-browserify'),
-        undici: false,
-      };
-    }
-    return config;
-  },
 }
 
 module.exports = nextConfig
